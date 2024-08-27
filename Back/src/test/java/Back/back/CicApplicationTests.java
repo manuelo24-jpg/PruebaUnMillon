fffe2d000f7425de86a41b774362ec7c1654cf63
Manuelo69 <nomail@gmail.com>
2024-08-27 10:16:52 +0200

    Primer commit, Ramiro te extraño

---
+++ Back/src/test/java/Back/back/CicApplicationTests.java
@@ -0,0 +1,13 @@
+package Back.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
